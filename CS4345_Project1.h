094a4c4050004fa6add5ed6392627773cb8145aa
Cutupnottoasted <Cnh71@txstate.edu>
2023-02-06 15:46:27 -0600

    Update the header

---
+++ CS4345_Project1.h
@@ -0,0 +1,56 @@
+// CS4345_PROJECT 1 By BeegAI
+// This project will diagnose what substance the patient has been poisioned by
+// The AI will diagnose the poison by using backwards chaining
+// and then will recommend the treatment through forward chaining
+// the AI will be supplemented by a nurse who will enter the symptoms of the patient. 
+// FUNCTIONS
+//
+//
+// search_con (double var):
+// finds the matching variable in conclusion list
+// and the corresponding rule number ri
+// calls rule_to_clause (int ri):
+//
+// rule_to_clause (int ri):
+// Converts ri to clause variable list number, cvi using the formula:
+// cvi = 4 * (ri / 10 - 1) + 1 *numbers are seq. 10,20,30..
+// check if cvi in variable list is instantiated
+// if not, then ask with update_VL(cvi)
+//
+// update_VL (int cvi):
+// checks if the cvi is avaliable to update, then prompt 
+// process(cvi) will define the value of the variable list
+// if cvi is not in variable list, or is already instantiated
+// then find a variable in the clause to instantiate
+//
+// validate_ri (int ri, string conclusion):
+// check if the variables in the 'if' clause of the rules
+// are satisfied. if yes then assign conclusion of rule (yes or no)
+// to the string 'conclusion' otherwise no value is assigned.
+
+#ifndef CS4345_PROJECT1_H
+#define CS4345_PROJECT1_H
+#include <iostream>
+
+class Patient 
+{
+    public:
+        // CONSTRUCTOR/DECONSTRUCTOR
+        static const int DEFAULT_CAPACITY = 3;
+        Patient(int initial_capacity = DEFAULT_CAPACITY);
+        ~Patient();
+        int size() const; // returns number of symptoms
+        bool isEmpty() const; // checks if there are symptoms
+        bool contains(string symptom)
+        void addSymptom(string symptom)
+        
+    private:
+        std::string* symptom_array; // dynamic array containing variable list
+        std::string poison; // holds the name of the poison
+        int capacity; // capacity of symptom_array
+        int used; // current number of symptoms
+        void resize(int new_capacity); 
+}
+
+
+#endif
